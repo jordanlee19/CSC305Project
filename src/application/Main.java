ead6cd7f494d3b888bbd1c9e1fa1ed63114923bd
rachelmontesano <90405612+rachelmontesano@users.noreply.github.com>
2021-10-13 23:24:13 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -44,6 +44,7 @@ public class Main extends Application {
 			Scene scene = new Scene(gridPane,300,140);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Schedule Visualizer");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();

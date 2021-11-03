867cbfa57da160e6dc40e6ad2830e67b2435e072
Rachel Montesano <rachelmontesano19@augustana.edu>
2021-11-03 16:28:47 -0500

    added code to Course

--- src/application/Course.java
+++ src/application/Course.java
@@ -1,5 +1,24 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Course {
+	
+	String courseCode;
+	String facultyName;
+	String location;
+	String courseName;
+	String days;
+	String time;
+	
+	public Course(ArrayList<String> courseList) {
+		courseCode = courseList.get(0);
+		facultyName = courseList.get(9);
+		location = courseList.get(7) + courseList.get(8);
+		courseName = courseList.get(1);
+		days = courseList.get(5);
+		time = courseList.get(6);
+		
+	}
 
 }

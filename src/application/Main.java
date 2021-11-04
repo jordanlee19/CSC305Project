52d3b9f56b1ceb57af7376a8ce68fd361fdbfa20
jordanlee19 <70791417+jordanlee19@users.noreply.github.com>
2021-11-04 09:39:30 -0500

    Updated main.
    Added code to support any amount of classes between 1 and 4.

--- src/application/Main.java
+++ src/application/Main.java
@@ -29,45 +29,104 @@ public class Main extends Application {
 					Stage newStage = new Stage();
 					GridPane grid2 = new GridPane();
 					String rawSched = schedule.getText();
-					ArrayList<String> schedList = new ArrayList<String>();
+					ArrayList<String> courseOne = new ArrayList<String>();
+					ArrayList<String> courseTwo = new ArrayList<String>();
+					ArrayList<String> courseThree = new ArrayList<String>();
+					ArrayList<String> courseFour = new ArrayList<String>();
+					int lineCount = 1;
+					String output = "These are your courses: ";
 					
 					//Iterates over the string line by line, not sure how to separate classes properly yet.
 					Iterable<String> sc = () ->
 					    new Scanner(rawSched).useDelimiter("\n");
+					
 					for (String line: sc) {
-
-						if (line.replaceAll("\\s", "").equals("")) { 
-							//do nothing
-						} else {
-							schedList.add(line);
+						if (lineCount <= 11) {
+							courseOne.add(line);
+							lineCount++;
+						} else if (lineCount > 11 && lineCount <= 22) {
+							courseTwo.add(line);
+							lineCount++;
+						} else if (lineCount > 22 && lineCount <= 33) {
+							courseThree.add(line);
+							lineCount++;
+						} else if (lineCount > 33 && lineCount <= 44) {
+							courseFour.add(line);
+							lineCount++;
 						}
 					}
 					
-					ArrayList<String> oneCourseList = new ArrayList<String>();
-					for (int i = 0; i < 10; i++) {
-						oneCourseList.add(schedList.get(i));
+					ArrayList<String> courseOneList = new ArrayList<String>();
+					ArrayList<String> courseTwoList = new ArrayList<String>();
+					ArrayList<String> courseThreeList = new ArrayList<String>();
+					ArrayList<String> courseFourList = new ArrayList<String>();
+					
+					if (lineCount >= 11) {
+						for (int i = 0; i < 11; i++) {
+							courseOneList.add(courseOne.get(i));
+						}
+					}
+					if (lineCount >= 22) {
+						for (int i = 0; i < 11; i++) {
+							courseTwoList.add(courseTwo.get(i));
+						}
+					}
+					if (lineCount >= 33) {
+						for (int i = 0; i < 11; i++) {
+							courseThreeList.add(courseThree.get(i));
+						}
+					}
+					if (lineCount >= 44) {
+						for (int i = 0; i < 11; i++) {
+							courseFourList.add(courseFour.get(i));
+						}
 					}
 					
-					Course courseOne = new Course(oneCourseList);
-					String output = courseOne.courseCode + "\n" + courseOne.courseName + "\n"
-							+ courseOne.days + "\n" + courseOne.facultyName + "\n" + courseOne.location
-							+ "\n" + courseOne.time;
+					int winSize = 0;
+					if (lineCount >= 11) {
+						Course courseOneObj = new Course(courseOneList);
+						winSize+=145;
+						output += "\n" + "\n" + courseOneObj.courseCode + "\n" + courseOneObj.courseName + "\n"
+								+ courseOneObj.days + "\n" + courseOneObj.facultyName + "\n" + courseOneObj.location
+								+ "\n" + courseOneObj.time;
+					}
+					if (lineCount >= 22) {
+						Course courseTwoObj = new Course(courseTwoList);
+						winSize+=125;
+						output += "\n" + "\n" + courseTwoObj.courseCode + "\n" + courseTwoObj.courseName + "\n"
+								+ courseTwoObj.days + "\n" + courseTwoObj.facultyName + "\n" + courseTwoObj.location
+								+ "\n" + courseTwoObj.time;
+					}
+					if (lineCount >= 33) {
+						Course courseThreeObj = new Course(courseThreeList);
+						winSize+=125;
+						output += "\n" + "\n" + courseThreeObj.courseCode + "\n" + courseThreeObj.courseName + "\n"
+								+ courseThreeObj.days + "\n" + courseThreeObj.facultyName + "\n" + courseThreeObj.location
+								+ "\n" + courseThreeObj.time;
+					}
+					if (lineCount >= 44) {
+						Course courseFourObj = new Course(courseFourList);
+						winSize+=125;
+						output += "\n" + "\n" + courseFourObj.courseCode + "\n" + courseFourObj.courseName + "\n"
+								+ courseFourObj.days + "\n" + courseFourObj.facultyName + "\n" + courseFourObj.location
+								+ "\n" + courseFourObj.time;
+					}
 					
 					//Creates the window
 					Text text2 = new Text(10,50,output);
 					grid2.setPadding(new Insets(10, 10, 10, 10));
 					grid2.setVgap(5);
 					grid2.setHgap(5);
-					newStage.setScene(new Scene(grid2, 300, 300));
+					newStage.setScene(new Scene(grid2, 300, winSize));
 					newStage.show();
 					grid2.add(text2, 0, 0);
+					newStage.setTitle("Schedule Visualizer");
 				}
 			});
 			
 			gridPane.setPadding(new Insets(10, 10, 10, 10));
 			gridPane.setVgap(5);
 			gridPane.setHgap(5);
-       
 			gridPane.add(schedule, 0, 0);
 			gridPane.add(submit, 0, 1);
 			

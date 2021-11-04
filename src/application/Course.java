7d4b788115ec5934a1c412da79bf7220d1faf335
jordanlee19 <70791417+jordanlee19@users.noreply.github.com>
2021-11-04 09:40:35 -0500

    Updated course class.
    Changed variable declaration in the constructor to correctly display room number, faculty name, and building name.

--- src/application/Course.java
+++ src/application/Course.java
@@ -13,8 +13,8 @@ public class Course {
 	
 	public Course(ArrayList<String> courseList) {
 		courseCode = courseList.get(0);
-		facultyName = courseList.get(9);
-		location = courseList.get(7) + courseList.get(8);
+		facultyName = courseList.get(10);
+		location = courseList.get(8) + " " + courseList.get(9);
 		courseName = courseList.get(1);
 		days = courseList.get(5);
 		time = courseList.get(6);

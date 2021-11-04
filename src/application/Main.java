7ff1b9aa0ef10a43c13dc16f76fd0bcc2a9e9d46
Rachel Montesano <rachelmontesano19@augustana.edu>
2021-11-03 19:44:39 -0500

    added code to Main, creates Course object

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javafx.application.Application;
@@ -28,7 +29,7 @@ public class Main extends Application {
 					Stage newStage = new Stage();
 					GridPane grid2 = new GridPane();
 					String rawSched = schedule.getText();
-					String visualText = "This is your schedule by line:";
+					ArrayList<String> schedList = new ArrayList<String>();
 					
 					//Iterates over the string line by line, not sure how to separate classes properly yet.
 					Iterable<String> sc = () ->
@@ -38,16 +39,26 @@ public class Main extends Application {
 						if (line.replaceAll("\\s", "").equals("")) { 
 							//do nothing
 						} else {
-							visualText += "\n" + line;
+							schedList.add(line);
 						}
 					}
 					
-					//Creates the window, not sure how to deal with schedule being super long?
-					Text text2 = new Text(10,50,visualText);
+					ArrayList<String> oneCourseList = new ArrayList<String>();
+					for (int i = 0; i < 10; i++) {
+						oneCourseList.add(schedList.get(i));
+					}
+					
+					Course courseOne = new Course(oneCourseList);
+					String output = courseOne.courseCode + "\n" + courseOne.courseName + "\n"
+							+ courseOne.days + "\n" + courseOne.facultyName + "\n" + courseOne.location
+							+ "\n" + courseOne.time;
+					
+					//Creates the window
+					Text text2 = new Text(10,50,output);
 					grid2.setPadding(new Insets(10, 10, 10, 10));
 					grid2.setVgap(5);
 					grid2.setHgap(5);
-					newStage.setScene(new Scene(grid2, 275, 950));
+					newStage.setScene(new Scene(grid2, 300, 300));
 					newStage.show();
 					grid2.add(text2, 0, 0);
 				}
@@ -60,7 +71,7 @@ public class Main extends Application {
 			gridPane.add(schedule, 0, 0);
 			gridPane.add(submit, 0, 1);
 			
-			Scene scene = new Scene(gridPane,300,140);
+			Scene scene = new Scene(gridPane,300,300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Schedule Visualizer");
